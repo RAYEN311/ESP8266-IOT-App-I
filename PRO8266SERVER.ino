2a29991785a947981832c5c85cb4b31a713de73f
Rayen ben brahim <129397918+RAYEN311@users.noreply.github.com>
2023-10-21 19:11:39 +0100

    Update PRO8266SERVER.ino

--- PRO8266SERVER.ino
+++ PRO8266SERVER.ino
@@ -1,5 +1,3 @@
-
-
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
@@ -15,11 +13,11 @@ const char* password = STAPSK;
 
 ESP8266WebServer server(80);
 
-const char* www_username = "admin";
-const char* www_password = "esp8266";
-// allows you to set the realm of authentication Default:"Login Required"
+const char* www_username = "RAYEN";
+const char* www_password = "RAYEN";
+
 const char* www_realm = "Custom Auth Realm";
-// the Content of the HTML response in case of Unautherized Access Default:empty
+
 String authFailResponse = "Authentication Failed";
 
 void setup() {
@@ -36,30 +34,18 @@ void setup() {
 
   server.on("/", []() {
     if (!server.authenticate(www_username, www_password))
-    // Basic Auth Method with Custom realm and Failure Response
-    // return server.requestAuthentication(BASIC_AUTH, www_realm, authFailResponse);
-    // Digest Auth Method with realm="Login Required" and empty Failure Response
-    // return server.requestAuthentication(DIGEST_AUTH);
-    // Digest Auth Method with Custom realm and empty Failure Response
-    // return server.requestAuthentication(DIGEST_AUTH, www_realm);
-    // Digest Auth Method with Custom realm and Failure Response
     {
       return server.requestAuthentication(DIGEST_AUTH, www_realm, authFailResponse);
     }
-    server.send(200, "text/plain", "Login OK rayen you are the best");
+    server.send(200, "text/plain", "Rayen you are the best");
   });
   server.begin();
-
   Serial.print("Open http://");
   Serial.print(WiFi.localIP());
 }
-
 void loop() {
   ArduinoOTA.handle();
   server.handleClient();
-
 }
-
 void sell_blink(){
-
 }
